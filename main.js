6e9eb2c7d0389ae123fa60fe920ed196005a41af
huntingbees <57939892+huntingbees@users.noreply.github.com>
2022-10-24 20:09:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-window.correct = 100;
+window.correct = 1814;
 window.levelStarted = Date.now();
 
 window.myUsername = $("#user_id").text();
-window.opUsername = "гопстоп";
 
 window.myAbs = -1;
 window.opAbs = -1;
 window.myDt = -1;
 window.opDt = -1;
 
+window.finish = function() {
+ enter(vic);
+ enter("up");
+}
+
 window.submitAnswerCallback = function (answer) {
 
   const obj = {
 
-    username: $("#user_id").text(),
+    user: myUsername,
     task_id: task_id,
     answer: answer,
     time: Date.now()
 
   };
 
-  connection.invoke("BroadcastMessage", myUsername, JSON.stringify(obj));
-
-  //  $("#output").append(answer + " " + Date.now() + "</br>"); 
-
+  connection.invoke("BroadcastMessage", team_id.toString(),  JSON.stringify(obj));
   console.log("on answer submit", answer);
 }
 
@@ -54,60 +55,68 @@ connection.on("send", data => {
   console.log(data);
 });
 
-connection.on("BroadcastMessage", (user, message) => {
-  console.log("SignalR ReceiveMessage.", user, message);
+connection.on("BroadcastMessage", (team, message) => {
+  console.log("SignalR ReceiveMessage.", team, message);
+  
+  if (team !== team_id.toString()) {
+   return;
+  }
 
+  const data = JSON.parse(message);
+  const user = data.user;
 
-  if (user === myUsername) {
-    const data = JSON.parse(message);
+  if (data.user === myUsername) {
     const abs = Math.abs(window.correct - data.answer);
     const dt = (data.time - window.levelStarted) / 1000.0;
-    $("#myinput").text(user + ": " + data.answer + " через " + dt + " секунд, разница: " + abs);
+    $("#myinput_short").text(data.answer);
+    $("#myinput").text(data.answer + " — " + " разница " + abs + " — " + dt + " секунд");
     myAbs = abs;
     myDt = dt;
   }
 
-  if (user === opUsername) {
-    const data = JSON.parse(message);
+  if (data.user !== myUsername) {
     const abs = Math.abs(window.correct - data.answer);
     const dt = (data.time - window.levelStarted) / 1000.0;
-    $("#opinput").text(user + ": " + data.answer + " через " + dt + " секунд, разница: " + abs);
+    $("#opinput").text(data.answer + " — " + " разница " + abs + " — " + dt + " секунд");
     opAbs = abs;
     opDt = dt;
   }
 
-  if ((myAbs > 0) && (opAbs > 0)) {
+  if ((myAbs > -1) && (opAbs > -1)) {
     if (myAbs < opAbs) {
-      $("#output").text(myUsername);
       window.vic = "myVic";
+      $("#output").text("Вы выиграли!");
     }
 
     if (myAbs > opAbs) {
-      $("#output").text(opUsername);
       window.vic = "opVic";
-      $("#up").attr("hidden", false);
+      $("#output").text("Вы проиграли!");
     }
 
     if (myAbs === opAbs) {
       if (myDt < opDt) {
-        $("#output").text(myUsername);
         window.vic = "myVic";
-        $("#up").attr("hidden", false);
+      $("#output").text("Вы выиграли!");
       }
 
       if (myDt > opDt) {
-        $("#output").text(opUsername);
         window.vic = "opVic";
-        $("#up").attr("hidden", false);
+      $("#output").text("Вы проиграли!");
       }
 
       if (myDt === opDt) {
-        $("#output").text("draw");
         window.vic = "draw";
-        $("#up").attr("hidden", false);
+      $("#output").text("Ничья!");
       }
-
     }
+    
+    $("#correct_answer").text("Ответ: " + window.correct);
+    $("#up").attr("hidden", false);
+    $("#opinput").attr("hidden", false);
+    $("#myinput").attr("hidden", false);
+    $("#myinput_short").attr("hidden", true);
+
+
   }
 
 });
